6b3c0612bea02288ba1d1ab053927f2a827ae51b
k-nh <skgml977@gmail.com>
2022-10-15 22:39:00 +0900

    CH07 실습 끝

---
+++ CH07/ManualCoreData/ManualCoreData/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ManualCoreData
+//
+//  Created by 김나희 on 10/15/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
